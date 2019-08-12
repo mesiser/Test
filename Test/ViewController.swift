357e5a608e9bcf78f3996f853f38e20edd271af8
Vadim Shalugin <vadim@MacBook-Pro-Vadim.local>
2019-08-12 14:53:12 +0300

    Initial commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Vadim Shalugin on 28/06/2019.
+//  Copyright © 2019 Vadim Shalugin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
